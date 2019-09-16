1f4892b8bdae2efd045d713386aec33328b30e25
everydaymorning <zmflrqll@naver.com>
2019-09-16 17:04:59 +0900

    test2

---
+++ algorithm_p/src/codeup/Code45.java
@@ -0,0 +1,21 @@
+package codeup;
+
+import java.util.Scanner;
+
+public class Code45 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		System.out.format("%.2f",(float)a/b);
+	}
+
+}
